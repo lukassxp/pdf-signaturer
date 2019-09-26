d5892807b3b3661b544c0dec9535805615c569c7
lukassxp <lpalucas.10@gmail.com>
2019-09-26 01:31:59 -0300

    configured gh-pages

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Main} />
+        <Route path="" exact component={Main} />
       </Switch>
     </BrowserRouter>
   );
